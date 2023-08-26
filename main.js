14b831ff47787eeacf7c8bb0af14342fc917d1f8
nachof948 <nachof948@gmail.com>
2023-08-26 15:57:32 -0300

    Entrega Final

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ window.addEventListener('scroll', () =>{
     if(window.scrollY > 1){
         header.style.backgroundColor = 'rgb(132,183,78)';
         header.style.padding= '0rem 0rem';
-
     }else{
         header.style.backgroundColor = 'transparent';
     }
