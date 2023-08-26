c8a4a72bab84be2517d99ecbd3046b38ef3a28b5
nachof948 <nachof948@gmail.com>
2023-08-26 14:48:28 -0300

    Entrega Final

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const contenedorHeader = document.getElementsByClassName('contenedor-header');
 window.addEventListener('scroll', () =>{
     if(window.scrollY > 1){
         header.style.backgroundColor = 'rgb(132,183,78)';
-        header.style.padding= '1rem 0rem';
+        header.style.padding= '0rem 0rem';
 
     }else{
         header.style.backgroundColor = 'transparent';
